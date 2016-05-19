cc3d99dd38bee86e200029282b584c60cd732f44
belheber <belksys@gmail.com>
2016-05-19 19:43:42 +0100

    mnimise waiting time

--- pkgs/webscrap/scraper.py
+++ pkgs/webscrap/scraper.py
@@ -10,12 +10,7 @@ url_p = ['http://medicalxpress.com/news/2016-05-pfizer-blocking-drugs-lethal.htm
 'http://www.naturalhealth365.com/blueberries-heart-health-1827.html',
 'http://www.eurekalert.org/pub_releases/2016-05/giot-lso050816.php',
 'http://www.eurekalert.org/pub_releases/2016-05/uop-oof050916.php',
-'http://www.eurekalert.org/pub_releases/2016-05/hzm--hfh050916.php',
-'http://www.eurekalert.org/pub_releases/2016-05/tiof-nfn050916.php',
-'http://www.eurekalert.org/pub_releases/2016-05/bumc-sfm050916.php',
-'http://www.newsweek.com/john-oliver-last-week-tonight-scientific-studies-457343?rx=us',
-'http://singularityhub.com/2016/05/11/five-exponential-trends-are-accelerating-robotics/',
-'http://singularityhub.com/2016/05/12/the-personal-factory-is-here-and-it-will-bring-a-wild-new-era-of-invention/']
+'http://www.eurekalert.org/pub_releases/2016-05/hzm--hfh050916.php']
 
 #url = 'http://www.newsweek.com/john-oliver-last-week-tonight-scientific-studies-457343?rx=us'
 
@@ -27,7 +22,7 @@ f.write("\n"+""" {% block content %} """)
 
 def scrape(link, attribute):
   driver.get(link)
-  sleep(3)
+#  sleep(3)
   s = BeautifulSoup(driver.page_source)
   data = s.find(attribute)
   formt_data = data.string.encode("cp437", "ignore")
